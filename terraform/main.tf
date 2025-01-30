bb8429cdf7c573ee0a63d5fae4fdd5b958095c81
saik90305@gmail.com <saik90305@gmail.com>
2025-01-30 12:49:25 +0530

    added terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ module "catalogue_instance" {
   instance_type          = "t3.medium"
   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
   #it should be in db subnet
-  subnet_id = split(data.aws_ssm_parameter.private_subnet_ids.value, 0)
-  user_data = file("catalogue.sh")
+  subnet_id = element(split(",", data.aws_ssm_parameter.private_subnet_ids.value), 0)
+  #user_data = file("catalogue.sh")
   tags = merge(
     {
       Name = "catalogue-DEV-AMI"
